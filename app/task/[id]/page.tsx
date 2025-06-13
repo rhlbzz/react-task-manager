dfe9a671c76e51353eb3f255b5cec93247f6e4d4
Rachele Bizzarri <rachebizz@gmail.com>
2025-06-14 01:02:27 +0200

    feat: updated TaskPageProps

--- app/task/[id]/page.tsx
+++ app/task/[id]/page.tsx
@@ -1,7 +1,7 @@
 import TaskItem from '../../src/components/task/TaskItem';
 
 interface TaskPageProps {
-  params: { id: string };
+  params: { [key: string]: string };
 }
 
 export default async function TaskPage({ params }: TaskPageProps) {
