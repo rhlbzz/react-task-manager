59fc77958ffb0c0541006bc15efab03aac763e05
Rachele Bizzarri <rachebizz@gmail.com>
2025-06-13 16:49:32 +0200

    feat: updates in CreateTask for new StatusComponent props

--- src/components/form/CreateTask.tsx
+++ src/components/form/CreateTask.tsx
@@ -1,44 +1,64 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CtaComponent from '../ui/CtaComponent';
-
+import { Status } from '@/contants';
+import { useRouter } from 'next/navigation';
 
 const CreateTaks: React.FC = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [isClient, setIsClient] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Recupera i task esistenti o crea array vuoto
+    if (!isClient) return;
     const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
     const newTask = {
       id: `task-${storedTasks.length + 1}`,
       title,
       description,
-      status: 'open',
+      status: Status.BACKLOG,
     };
-
-    // Aggiungi il nuovo task
     const updatedTasks = [...storedTasks, newTask];
-
-    // Salva in localStorage
     localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-
-    // Reset dei campi (opzionale)
     setTitle('');
     setDescription('');
     console.log('Task created:', newTask);
+    router.push('/');
   };
+
+  if (!isClient) return null;
 
   return (
     <form className="space-y-4" onSubmit={handleSubmit}>
       <div>
         <label htmlFor="title" className="block text-sm font-medium text-gray-700">Task Title</label>
-        <input type="text" id="title" name="title" required className="mt-1 block w-full border-gray-950 rounded-md shadow-sm" onChange={(e) => (setTitle(e.target.value))}/>
+        <input 
+          type="text" 
+          id="title" 
+          name="title" 
+          required 
+          className="mt-1 block w-full border-gray-950 rounded-md shadow-sm" 
+          value={title}
+          onChange={(e) => (setTitle(e.target.value))}
+        />
       </div>
       <div>
         <label htmlFor="description" className="block text-sm font-medium text-gray-700">Description</label>
-        <textarea id="description" name="description" rows={4} required className="mt-1 block w-full border-gray-300 rounded-md shadow-sm" onChange={(e) => (setDescription(e.target.value))}></textarea>
+        <textarea 
+          id="description" 
+          name="description" 
+          rows={4} 
+          required 
+          className="mt-1 block w-full border-gray-300 rounded-md shadow-sm" 
+          value={description}
+          onChange={(e) => (setDescription(e.target.value))}
+        ></textarea>
       </div>
       <div>
         <CtaComponent
