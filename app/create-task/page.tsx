90c424059d36edf3584f471f37dbeb985b178ec5
Rachele Bizzarri <rachebizz@gmail.com>
2025-06-13 16:33:04 +0200

    feat: set /create-task page

---
+++ app/create-task/page.tsx
@@ -0,0 +1,11 @@
+import CreateTaks from '@/src/components/form/CreateTask';
+import React from 'react';
+export default function CreateTask() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">New Task</h1>
+      <hr />
+      <CreateTaks />
+    </div>
+  );
+}
