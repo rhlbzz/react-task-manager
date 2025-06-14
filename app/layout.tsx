c12b622353e0e9fe31bd1f4776d8a8a86d3170d7
Rachele Bizzarri <rachebizz@gmail.com>
2025-06-14 20:36:03 +0200

    feat: add shared image and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,19 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "React Task Manager",
-  description: "Mangage yoir tasks with this simple app!",
+  description: "Manage your tasks with this simple app!",
+  openGraph: {
+    title: "React Task Manager",
+    description: "Manage your tasks with this simple app!",
+    images: [
+      {
+        url: '/shared.jpg',
+        width: 1440,
+        height: 800,
+        alt: 'React Task Manager',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -28,6 +40,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
